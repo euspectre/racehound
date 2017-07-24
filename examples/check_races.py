d8a35fc812367ff79b5d8013dd9c67b328663c6e
Evgenii Shatokhin <eugene.shatokhin@rosalab.ru>
2017-07-24 17:08:30 +0300

    examples: Adapted the scripts because race events are no longer reported
    Races are only reported to the system log (dmesg) now.

--- examples/check_races.py
+++ examples/check_races.py
@@ -23,15 +23,17 @@
 #
 # All this allows to check all the given instructions in time.
 #
+# If RaceHound finds races, it reports them to the system log (dmesg). This
+# script will not be notified of such events.
+#
 # Note. Python 3.4 or newer is required.
 #
 # If -v (--verbose) is present, the script will output more info as it
-# works. Without it, only the summary of the found races is output at the
-# exit as well as the errors.
+# works.
 #
 # The script assumes debugfs is mounted to /sys/kernel/debug/.
 #
-# Note. Per-breakpoint delays ('delay=...' in the beakpoint specification)
+# Note. Per-breakpoint delays ('delay=...' in the breakpoint specification)
 # are not supported here. Use the parameters of the kernel part of RaceHound
 # to set the delays globally if needed.
 
@@ -51,17 +53,6 @@ EVENTS_FILE = '/sys/kernel/debug/racehound/events'
 ERR_NO_FILES = ''.join([
     'Please check that debugfs is mounted to /sys/kernel/debug ',
     'and kernel module \"racehound\" is loaded.'])
-
-RE_RACE = re.compile(' '.join([
-    r'Detected a data race on the memory block at (0x)?[0-9a-f]+',
-    r'between the instruction at ([^ \t]+) \(comm: \"(.+)\"\)',
-    r'and the instruction right before (.*) \(comm: \"(.+)\"\)']))
-
-RE_RREAD = re.compile(' '.join([
-    r'Detected a data race on the memory block at (0x)?[0-9a-f]+',
-    r'that is about to be accessed by the instruction at ([^ \t]+)',
-    r'\(comm: \"(.+)\"\):',
-    r'the memory block was modified during the delay']))
 
 # [<module_name>:]{init|core}+0xoffset
 RE_BP_SPEC = re.compile(r'^([^:]+:)?(core|init)\+0x[a-f0-9]{1,8}$')
@@ -93,82 +84,6 @@ def positive_int(string):
         msg = "%r is not a positive integer" % string
         raise argparse.ArgumentTypeError(msg)
     return value
-
-
-class RaceGroup(object):
-    '''A group of races between a given pair of instructions
-
-    'insn' - address of the instruction that under watch,
-    'insn_comm' - 'comm' of the process that executed 'insn',
-    'conflict_insn' - the address right after the instruction that performed
-        a conflicting memory access (None if unknown),
-    'conflict_comm' - 'comm' of the process that executed 'conflict_insn'
-        (None if unknown).
-    '''
-    def __init__(self, insn, insn_comm, conflict_insn=None,
-                 conflict_comm=None):
-        self.insn = insn
-        self.insn_comms = [insn_comm]
-        self.conflict_insn = conflict_insn
-        if conflict_comm:
-            self.conflict_comms = [conflict_comm]
-        else:
-            self.conflict_comms = []
-
-        # How many times this race was reported.
-        self.count = 1
-
-    def print_races(self):
-        if self.conflict_insn:
-            print('Race between %s and the insn right before %s.' %
-                  (self.insn, self.conflict_insn))
-            comms = list(set(self.insn_comms))
-            print('The first insn was executed by:', ', '.join(comms))
-            comms = list(set(self.conflict_comms))
-            print('The second insn was executed by:', ', '.join(comms))
-        else:
-            print('Race between %s and some other code.' % self.insn)
-            comms = list(set(self.insn_comms))
-            print('The insn was executed by:', ', '.join(comms))
-
-        print('The race was reported %d time(s).' % self.count)
-
-
-def store_race_info(races, str_race):
-    matched = re.search(RE_RACE, str_race)
-    if matched:
-        _, insn, insn_comm, conflict_insn, conflict_comm = matched.groups()
-        key = insn + '#' + conflict_insn
-    else:
-        matched = re.search(RE_RREAD, str_race)
-        if not matched:
-            sys.stderr.write(
-                'Unknown format of a race report: "%s".\n' % str_race)
-            return
-
-        _, insn, insn_comm = matched.groups()
-        conflict_insn = None
-        conflict_comm = None
-        key = insn
-
-    if key in races:
-        races[key].count = races[key].count + 1
-        races[key].insn_comms.append(insn_comm)
-        if conflict_comm:
-            races[key].conflict_comms.append(conflict_comm)
-    else:
-        races[key] = RaceGroup(
-            insn, insn_comm, conflict_insn, conflict_comm)
-
-
-def print_summary(races):
-    if not races:
-        print('No races found.')
-    else:
-        for _, grp in races.items():
-            grp.print_races()
-            print('')
-
 
 def place_bp(bp, bps):
     '''Tell RaceHound to place the given BP and mark the BP as set.
@@ -219,7 +134,7 @@ def replace_bp(bp, bps, hit_rate):
 
 def add_bps(num, bps):
     '''Select the given number of BPs randomly and try to set them.'''
-    available = [bp for bp in bps.keys() if bps[bp] == False]
+    available = [bp for bp in bps.keys() if not bps[bp]]
     if not available:
         if verbose:
             print('No breakpoints can be set.\n')
@@ -269,9 +184,6 @@ if __name__ == '__main__':
         raise ValueError(
             'max_hit_rate should be greater than min_hit_rate.')
 
-    # Mapping: {racing_insns => race_info}
-    races = {}
-
     # Mapping BP=>bool (True - BP is set, False - not set)
     bps = {}
 
@@ -317,13 +229,6 @@ if __name__ == '__main__':
                 events = sel.select(timeout=HIT_CHECK_INTERVAL)
                 for key, mask in events:
                     for line in f:
-                        if line.startswith('[race]'):
-                            line = line.rstrip()
-                            store_race_info(races, line)
-                            if verbose:
-                                print(line)
-                            continue
-
                         bp = line.rstrip()
                         if verbose:
                             print('BP hit:', bp)
@@ -342,5 +247,4 @@ if __name__ == '__main__':
                     hits = 0
 
         except KeyboardInterrupt:
-            print_summary(races)
             sys.exit(1)
